2218341da93d598f872b46cae7c60cb6dd97c41d
James H. Hill <hilljh82@gmail.com>
2017-07-19 17:22:43 -0400

    Fixed justgage inclusion

--- blueprints/ember-cli-justgage/index.js
+++ blueprints/ember-cli-justgage/index.js
@@ -4,6 +4,6 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addBowerPackageToProject ('https://github.com/toorshia/justgage.git', '1.2.9');
+    return this.addBowerPackageToProject ('justgage', 'https://github.com/toorshia/justgage.git#1.2.9');
   }
 };
