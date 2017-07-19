5f2d356ab19e02ba0819c987f9b3a3e376303754
James H. Hill <hilljh82@gmail.com>
2017-07-19 16:52:05 -0400

    Updated add-on bower packages

--- blueprints/ember-cli-justgage/index.js
+++ blueprints/ember-cli-justgage/index.js
@@ -4,9 +4,6 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall () {
-    return this.addBowerPackageToProject ([
-      {name: 'https://github.com/toorshia/justgage.git', target: '1.2.9'},
-      {name: 'raphael', target: '2.2.7'}
-    ]);
+    return this.addBowerPackageToProject ('https://github.com/toorshia/justgage.git', '1.2.9');
   }
 };
